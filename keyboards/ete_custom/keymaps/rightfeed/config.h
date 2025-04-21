e01109daa83ad8ba245aefd01061573608deeb1f
junichi.baba <junichi.baba@stylez.co.jp>
2025-04-22 00:22:14 +0900

    不要ファイル削除

--- keyboards/ete_custom/keymaps/rightfeed/config.h
+++
@@ -1,4 +0,0 @@
-// Copyright 2023 YMGWorks (@YMGWorks)
-// SPDX-License-Identifier: GPL-2.0-or-later
-
-#define MASTER_RIGHT
--- keyboards/ete_custom/rules.mk
+++
@@ -1,7 +0,0 @@
-SPLIT_KEYBOARD = yes
-VIA_ENABLE = yes
-LTO_ENABLE = yes
-ENCODER_ENABLE = yes
-RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
-RGBLIGHT_LIMIT_VAL = 255
-SPLIT_KEYBOARD = yes
