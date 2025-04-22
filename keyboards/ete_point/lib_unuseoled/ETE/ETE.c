f243a81163ba1c05bf4ecbaa9377c5cdbd8e4e2d
junichi.baba <junichi.baba@stylez.co.jp>
2025-04-22 12:05:26 +0900

    修正

--- keyboards/ete_point/lib_unuseoled/ETE/ETE.c
+++ keyboards/ete_point/lib_unuseoled/ETE/ETE.c
@@ -54,7 +54,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 #include "ETE.h"
-#include "drivers/pmw3360/pmw3360.h"
+#include "../../../../drivers/pmw3360/pmw3360.h"
 //#include "pmw3360.h"
 
 const uint8_t CPI_DEFAULT    = ETE_CPI_DEFAULT / 100;
