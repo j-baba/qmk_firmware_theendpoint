ca173a4848d63342902de246cd892b2f72319c06
junichi.baba <junichi.baba@stylez.co.jp>
2025-04-22 10:53:27 +0900

    修正

--- keyboards/ete_point/theendpoint/keymaps/trackball_RF/rules.mk
+++ keyboards/ete_point/theendpoint/keymaps/trackball_RF/rules.mk
@@ -4,11 +4,11 @@ MOUSEKEY_ENABLE = yes
 # Optical sensor driver for trackball.
 POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = custom
-SRC += matrix.c
 SRC += drivers/pmw3360/pmw3360.c
+
 QUANTUM_LIB_SRC += spi_master.c # Optical sensor use SPI to communicate
 
 # Include common library
 SRC += ../lib_unuseoled/ETE/ETE.c
 
-EXTRAFLAGS += -I../../../drivers/pmw3360
+INC += ../../../drivers/pmw3360
