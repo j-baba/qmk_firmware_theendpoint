59cc7040848b45d7a96f9ca37371acb4790791d6
junichi.baba <junichi.baba@stylez.co.jp>
2025-04-22 09:12:49 +0900

    pmw3360のライブラリパス修正

--- keyboards/ete_point/theendpoint/keymaps/trackball_RF/rules.mk
+++ keyboards/ete_point/theendpoint/keymaps/trackball_RF/rules.mk
@@ -5,7 +5,8 @@ MOUSEKEY_ENABLE = yes
 POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = custom
 SRC += drivers/pmw3360/pmw3360.c
+SRC += $(PROJECT_ROOT)/drivers/pmw3360
 QUANTUM_LIB_SRC += spi_master.c # Optical sensor use SPI to communicate
 
 # Include common library
-SRC += lib_unuseoled/ETE/ETE.c
+SRC += ../lib_unuseoled/ETE/ETE.c
